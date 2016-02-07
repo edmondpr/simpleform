aa483564f25236e349255aa8ea4462350f950c57
Edmond Pruteanu <edmondpr@gmail.com>
2016-02-07 23:29:37 +0000

    Connected table to Parse
    # Conflicts:
    #	ClientAppAndroid/.idea/gradle.xml
    #	ClientAppAndroid/.idea/misc.xml

--- ReceiverApp/app/queueTable/queueTable.js
+++ ReceiverApp/app/queueTable/queueTable.js
@@ -8,9 +8,9 @@ angular.module('simpleFormApp.queueTable', [])
     $scope.rowCollection = [];
     $scope.displayedCollection = [];
     
-    $scope.columns = ['label', 'value', 'position'];
+    $scope.columns = ['clientName', 'formOwner', 'formType'];
     
-	$http.get('https://api.parse.com/1/classes/ClientsFields', {
+	$http.get('https://api.parse.com/1/classes/PrintQueue', {
 	    headers: {
 	        'X-Parse-Application-Id': "HxlZ3d7O3BuGM6oION0qPLrtrh5TcqnGR1eRecmA",
 	        'X-Parse-REST-API-Key': "CaQYDxjD1iuYBOh8A0l3HNsDoNqbTSnZVbUzYHrK"
@@ -19,7 +19,7 @@ angular.module('simpleFormApp.queueTable', [])
 		for (var i in response.data.results) {
         	$scope.rowCollection.push(response.data.results[i]);
     	}
-    	$scope.displayedCollection  = [].concat($scope.rowCollection);
+    	$scope.displayedCollection  = $scope.rowCollection;
     });  
 
 });
